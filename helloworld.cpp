c295b48312156e7ef590ce908f75cac696e3e421
Codiecher <49608838+Codiecher@users.noreply.github.com>
2019-12-03 14:43:46 +0800

    Update helloworld.cpp

--- helloworld.cpp
+++ helloworld.cpp
@@ -2,9 +2,17 @@
 
 using namespace std;
 
+int x=0;
+
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+   x++;
+   if(x==1){
+       HappyBoi();
+   }
 }
 
+int HappyBoi(){
+    cout << "Good luck to me during Google Code-In!" << endl;
+    return 0;
+}
